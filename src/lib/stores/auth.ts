dbfad55c35b229f7812c46ecd68565215bf2dfad
alain-nambi <mihaja@phidia.onmicrosoft.com>
2026-02-25 10:07:03 +0300

    Suite

--- src/lib/stores/auth.ts
+++ src/lib/stores/auth.ts
@@ -1,14 +1,36 @@
+// import { writable } from "svelte/store";
+
+// const storedToken = localStorage.getItem("token");
+
+// export const authToken = writable<string | null>(storedToken);
+
+// authToken.subscribe((value) => {
+//   if (value) {
+//     localStorage.setItem("token", value);
+//   } else {
+//     localStorage.removeItem("token");
+//   }
+// });
+
+// export function logout() {
+//   authToken.set(null);
+// }
 import { writable } from "svelte/store";
 
-const storedToken = localStorage.getItem("token");
+const storedToken =
+  typeof window !== "undefined"
+    ? sessionStorage.getItem("token")
+    : null;
 
 export const authToken = writable<string | null>(storedToken);
 
 authToken.subscribe((value) => {
+  if (typeof window === "undefined") return;
+
   if (value) {
-    localStorage.setItem("token", value);
+    sessionStorage.setItem("token", value);
   } else {
-    localStorage.removeItem("token");
+    sessionStorage.removeItem("token");
   }
 });
 
